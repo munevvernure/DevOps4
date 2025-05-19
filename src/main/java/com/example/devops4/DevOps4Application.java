b55091e10255a52e1ca481355a5c48a98f301949
Münevver Eraslan <munevvernureraslan@gmail.com>
2025-05-19 16:29:43 +0300

    Project skeleton is created

---
+++ src/main/java/com/example/devops4/DevOps4Application.java
@@ -0,0 +1,13 @@
+package com.example.devops4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOps4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevOps4Application.class, args);
+    }
+
+}
